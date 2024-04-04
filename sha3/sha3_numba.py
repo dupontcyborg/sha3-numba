8febef42952236251318d5d489cacbf1f212bf75
nicolas <ndupont@cyborg.co>
2024-04-04 17:25:33 -0400

    fully unrolled keccak permutation function for performance

--- sha3/sha3_numba.py
+++ sha3/sha3_numba.py
@@ -3,18 +3,6 @@
 import numpy as np
 from numba import njit
 
-_KECCAK_RHO = np.array([
-     1,  3,  6, 10, 15, 21,
-    28, 36, 45, 55,  2, 14,
-    27, 41, 56,  8, 25, 43,
-    62, 18, 39, 61, 20, 44],
-    dtype=np.uint64)
-_KECCAK_PI = np.array([
-    10,  7, 11, 17, 18, 3,
-     5, 16,  8, 21, 24, 4,
-    15, 23, 19, 13, 12, 2,
-    20, 14, 22,  9,  6, 1],
-    dtype=np.uint64)
 _KECCAK_RC = np.array([
   0x0000000000000001, 0x0000000000008082,
   0x800000000000808a, 0x8000000080008000,
@@ -40,37 +28,161 @@ def _rol(x, s):
 @njit
 def _keccak_f(state):
     """
-    The keccak_f permutation function.
+    The keccak_f permutation function, unrolled for performance.
 
     """
 
     bc = np.zeros(5, dtype=np.uint64)
 
     for i in range(24):
-        # Parity
-        for x in range(5):
-            bc[x] = 0
-            for y in range(0, 25, 5):
-                bc[x] ^= state[x + y]
 
-        # Theta
-        for x in range(5):
-            t = bc[(x + 4) % 5] ^ _rol(bc[(x + 1) % 5], 1)
-            for y in range(0, 25, 5):
-                state[y + x] ^= t
+        # Parity calculation unrolled
+        bc[0] = state[0] ^ state[5] ^ state[10] ^ state[15] ^ state[20]
+        bc[1] = state[1] ^ state[6] ^ state[11] ^ state[16] ^ state[21]
+        bc[2] = state[2] ^ state[7] ^ state[12] ^ state[17] ^ state[22]
+        bc[3] = state[3] ^ state[8] ^ state[13] ^ state[18] ^ state[23]
+        bc[4] = state[4] ^ state[9] ^ state[14] ^ state[19] ^ state[24]
 
-        # Rho and pi
-        t = state[1]
-        for x in range(24):
-            bc[0] = state[_KECCAK_PI[x]]
-            state[_KECCAK_PI[x]] = _rol(t, _KECCAK_RHO[x])
-            t = bc[0]
+        # Theta unrolled
+        t0 = bc[4] ^ _rol(bc[1], 1)
+        t1 = bc[0] ^ _rol(bc[2], 1)
+        t2 = bc[1] ^ _rol(bc[3], 1)
+        t3 = bc[2] ^ _rol(bc[4], 1)
+        t4 = bc[3] ^ _rol(bc[0], 1)
 
-        for y in range(0, 25, 5):
-            for x in range(5):
-                bc[x] = state[y + x]
-            for x in range(5):
-                state[y + x] = bc[x] ^ ((~bc[(x + 1) % 5]) & bc[(x + 2) % 5])
+        state[0] ^= t0
+        state[5] ^= t0
+        state[10] ^= t0
+        state[15] ^= t0
+        state[20] ^= t0
+
+        state[1] ^= t1
+        state[6] ^= t1
+        state[11] ^= t1
+        state[16] ^= t1
+        state[21] ^= t1
+
+        state[2] ^= t2
+        state[7] ^= t2
+        state[12] ^= t2
+        state[17] ^= t2
+        state[22] ^= t2
+
+        state[3] ^= t3
+        state[8] ^= t3
+        state[13] ^= t3
+        state[18] ^= t3
+        state[23] ^= t3
+
+        state[4] ^= t4
+        state[9] ^= t4
+        state[14] ^= t4
+        state[19] ^= t4
+        state[24] ^= t4
+
+        # Rho and Pi unrolled
+        t1  = _rol(state[1], 1)
+        t2  = _rol(state[10], 3)
+        t3  = _rol(state[7], 6)
+        t4  = _rol(state[11], 10)
+        t5  = _rol(state[17], 15)
+        t6  = _rol(state[18], 21)
+        t7  = _rol(state[3], 28)
+        t8  = _rol(state[5], 36)
+        t9  = _rol(state[16], 45)
+        t10 = _rol(state[8], 55)
+        t11 = _rol(state[21], 2)
+        t12 = _rol(state[24], 14)
+        t13 = _rol(state[4], 27)
+        t14 = _rol(state[15], 41)
+        t15 = _rol(state[23], 56)
+        t16 = _rol(state[19], 8)
+        t17 = _rol(state[13], 25)
+        t18 = _rol(state[12], 43)
+        t19 = _rol(state[2], 62)
+        t20 = _rol(state[20], 18)
+        t21 = _rol(state[14], 39)
+        t22 = _rol(state[22], 61)
+        t23 = _rol(state[9], 20)
+        t24 = _rol(state[6], 44)
+
+        state[10] = t1
+        state[7]  = t2
+        state[11] = t3
+        state[17] = t4
+        state[18] = t5
+        state[3]  = t6
+        state[5]  = t7
+        state[16] = t8
+        state[8]  = t9
+        state[21] = t10
+        state[24] = t11
+        state[4]  = t12
+        state[15] = t13
+        state[23] = t14
+        state[19] = t15
+        state[13] = t16
+        state[12] = t17
+        state[2]  = t18
+        state[20] = t19
+        state[14] = t20
+        state[22] = t21
+        state[9]  = t22
+        state[6]  = t23
+        state[1]  = t24
+
+        # Chi unrolled
+        t0  = state[0] ^ ((~state[1]) & state[2])
+        t1  = state[1] ^ ((~state[2]) & state[3])
+        t2  = state[2] ^ ((~state[3]) & state[4])
+        t3  = state[3] ^ ((~state[4]) & state[0])
+        t4  = state[4] ^ ((~state[0]) & state[1])
+        t5  = state[5] ^ ((~state[6]) & state[7])
+        t6  = state[6] ^ ((~state[7]) & state[8])
+        t7  = state[7] ^ ((~state[8]) & state[9])
+        t8  = state[8] ^ ((~state[9]) & state[5])
+        t9  = state[9] ^ ((~state[5]) & state[6])
+        t10 = state[10] ^ ((~state[11]) & state[12])
+        t11 = state[11] ^ ((~state[12]) & state[13])
+        t12 = state[12] ^ ((~state[13]) & state[14])
+        t13 = state[13] ^ ((~state[14]) & state[10])
+        t14 = state[14] ^ ((~state[10]) & state[11])
+        t15 = state[15] ^ ((~state[16]) & state[17])
+        t16 = state[16] ^ ((~state[17]) & state[18])
+        t17 = state[17] ^ ((~state[18]) & state[19])
+        t18 = state[18] ^ ((~state[19]) & state[15])
+        t19 = state[19] ^ ((~state[15]) & state[16])
+        t20 = state[20] ^ ((~state[21]) & state[22])
+        t21 = state[21] ^ ((~state[22]) & state[23])
+        t22 = state[22] ^ ((~state[23]) & state[24])
+        t23 = state[23] ^ ((~state[24]) & state[20])
+        t24 = state[24] ^ ((~state[20]) & state[21])
+
+        state[0]  = t0
+        state[1]  = t1
+        state[2]  = t2
+        state[3]  = t3
+        state[4]  = t4
+        state[5]  = t5
+        state[6]  = t6
+        state[7]  = t7
+        state[8]  = t8
+        state[9]  = t9
+        state[10] = t10
+        state[11] = t11
+        state[12] = t12
+        state[13] = t13
+        state[14] = t14
+        state[15] = t15
+        state[16] = t16
+        state[17] = t17
+        state[18] = t18
+        state[19] = t19
+        state[20] = t20
+        state[21] = t21
+        state[22] = t22
+        state[23] = t23
+        state[24] = t24
 
         state[0] ^= _KECCAK_RC[i]
 
