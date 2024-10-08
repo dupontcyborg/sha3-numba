19f51755348b119839bf9ef1f30f85f7146b4681
nicolas <ndupont@cyborg.co>
2024-10-08 13:45:51 -0400

    Moved SHA-3 Python to unrolled functional style for fair benchmarking

--- sha3/sha3_python.py
+++ sha3/sha3_python.py
@@ -1,17 +1,8 @@
-"""SHA3 implementation in Python in OOP style"""
+"""SHA3 implementation in Python in functional style"""
 
 import numpy as np
 
-_KECCAK_RHO = [
-     1,  3,  6, 10, 15, 21,
-    28, 36, 45, 55,  2, 14,
-    27, 41, 56,  8, 25, 43,
-    62, 18, 39, 61, 20, 44]
-_KECCAK_PI = [
-    10,  7, 11, 17, 18, 3,
-     5, 16,  8, 21, 24, 4,
-    15, 23, 19, 13, 12, 2,
-    20, 14, 22,  9,  6, 1]
+# Keccak round constants
 _KECCAK_RC = np.array([
   0x0000000000000001, 0x0000000000008082,
   0x800000000000808a, 0x8000000080008000,
@@ -27,191 +18,344 @@ _KECCAK_RC = np.array([
   0x0000000080000001, 0x8000000080008008],
   dtype=np.uint64)
 
-_SPONGE_ABSORBING = 1
-_SPONGE_SQUEEZING = 2
+# Domain separation byte
 _DSBYTE = 0x06
 
-def rol(x, s):
-    """Rotate x left by s."""
-    return ((np.uint64(x) << np.uint64(s)) ^ (np.uint64(x) >> np.uint64(64 - s)))
+# Number of Keccak rounds
+_NUM_ROUNDS = 24
 
-class Sha3(object):
+def _rol(x, s):
     """
-    Implementation of the SHA-3 hash function.
+    Rotates x left by s
 
     Args:
-        bit_length (int): The desired bit length of the hash output.
-        b (bytes): The input data to be hashed.
+        x (int): The input value to rotate
+        s (int): The number of bits to rotate by
 
-    Raises:
-        ValueError: If the `bit_length` is not a valid value.
+    Returns:
+        int: The rotated value
+    """
+    return ((np.uint64(x) << np.uint64(s)) ^ (np.uint64(x) >> np.uint64(64 - s)))
 
-    Attributes:
-        bit_length (int): The bit length of the hash output.
-        rate (int): The rate of the sponge construction.
-        dsbyte (int): The domain separation byte.
-        i (int): The index of the current position in the buffer.
-        state (numpy.ndarray): The internal state of the hash function.
-        buf (numpy.ndarray): The buffer used for absorbing and squeezing.
-        direction (int): The direction of the sponge construction.
+def _keccak_f(state):
+    """
+    The keccak_f permutation function, unrolled for performance
 
-    Methods:
-        update(b): Absorb additional input data.
-        digest(): Return the hash value as bytes.
-        hexdigest(): Return the hash value as a hexadecimal string.
+    Args:
+        state (np.ndarray): The state array of the SHA-3 sponge construction
+
+    Returns:
+        np.ndarray: The permuted state array
+    """
+    # Temporary array for calculations
+    bc = np.zeros(5, dtype=np.uint64)
+
+    # 24 rounds of permutation
+    for i in range(_NUM_ROUNDS):
+
+        # Parity calculation unrolled
+        bc[0] = state[0] ^ state[5] ^ state[10] ^ state[15] ^ state[20]
+        bc[1] = state[1] ^ state[6] ^ state[11] ^ state[16] ^ state[21]
+        bc[2] = state[2] ^ state[7] ^ state[12] ^ state[17] ^ state[22]
+        bc[3] = state[3] ^ state[8] ^ state[13] ^ state[18] ^ state[23]
+        bc[4] = state[4] ^ state[9] ^ state[14] ^ state[19] ^ state[24]
+
+        # Theta unrolled
+        t0 = bc[4] ^ _rol(bc[1], 1)
+        t1 = bc[0] ^ _rol(bc[2], 1)
+        t2 = bc[1] ^ _rol(bc[3], 1)
+        t3 = bc[2] ^ _rol(bc[4], 1)
+        t4 = bc[3] ^ _rol(bc[0], 1)
+
+        state[0] ^= t0
+        state[5] ^= t0
+        state[10] ^= t0
+        state[15] ^= t0
+        state[20] ^= t0
+
+        state[1] ^= t1
+        state[6] ^= t1
+        state[11] ^= t1
+        state[16] ^= t1
+        state[21] ^= t1
+
+        state[2] ^= t2
+        state[7] ^= t2
+        state[12] ^= t2
+        state[17] ^= t2
+        state[22] ^= t2
+
+        state[3] ^= t3
+        state[8] ^= t3
+        state[13] ^= t3
+        state[18] ^= t3
+        state[23] ^= t3
+
+        state[4] ^= t4
+        state[9] ^= t4
+        state[14] ^= t4
+        state[19] ^= t4
+        state[24] ^= t4
+
+        # Rho and Pi unrolled
+        t1  = _rol(state[1], 1)
+        t2  = _rol(state[10], 3)
+        t3  = _rol(state[7], 6)
+        t4  = _rol(state[11], 10)
+        t5  = _rol(state[17], 15)
+        t6  = _rol(state[18], 21)
+        t7  = _rol(state[3], 28)
+        t8  = _rol(state[5], 36)
+        t9  = _rol(state[16], 45)
+        t10 = _rol(state[8], 55)
+        t11 = _rol(state[21], 2)
+        t12 = _rol(state[24], 14)
+        t13 = _rol(state[4], 27)
+        t14 = _rol(state[15], 41)
+        t15 = _rol(state[23], 56)
+        t16 = _rol(state[19], 8)
+        t17 = _rol(state[13], 25)
+        t18 = _rol(state[12], 43)
+        t19 = _rol(state[2], 62)
+        t20 = _rol(state[20], 18)
+        t21 = _rol(state[14], 39)
+        t22 = _rol(state[22], 61)
+        t23 = _rol(state[9], 20)
+        t24 = _rol(state[6], 44)
+
+        state[10] = t1
+        state[7]  = t2
+        state[11] = t3
+        state[17] = t4
+        state[18] = t5
+        state[3]  = t6
+        state[5]  = t7
+        state[16] = t8
+        state[8]  = t9
+        state[21] = t10
+        state[24] = t11
+        state[4]  = t12
+        state[15] = t13
+        state[23] = t14
+        state[19] = t15
+        state[13] = t16
+        state[12] = t17
+        state[2]  = t18
+        state[20] = t19
+        state[14] = t20
+        state[22] = t21
+        state[9]  = t22
+        state[6]  = t23
+        state[1]  = t24
+
+        # Chi unrolled
+        t0  = state[0] ^ ((~state[1]) & state[2])
+        t1  = state[1] ^ ((~state[2]) & state[3])
+        t2  = state[2] ^ ((~state[3]) & state[4])
+        t3  = state[3] ^ ((~state[4]) & state[0])
+        t4  = state[4] ^ ((~state[0]) & state[1])
+        t5  = state[5] ^ ((~state[6]) & state[7])
+        t6  = state[6] ^ ((~state[7]) & state[8])
+        t7  = state[7] ^ ((~state[8]) & state[9])
+        t8  = state[8] ^ ((~state[9]) & state[5])
+        t9  = state[9] ^ ((~state[5]) & state[6])
+        t10 = state[10] ^ ((~state[11]) & state[12])
+        t11 = state[11] ^ ((~state[12]) & state[13])
+        t12 = state[12] ^ ((~state[13]) & state[14])
+        t13 = state[13] ^ ((~state[14]) & state[10])
+        t14 = state[14] ^ ((~state[10]) & state[11])
+        t15 = state[15] ^ ((~state[16]) & state[17])
+        t16 = state[16] ^ ((~state[17]) & state[18])
+        t17 = state[17] ^ ((~state[18]) & state[19])
+        t18 = state[18] ^ ((~state[19]) & state[15])
+        t19 = state[19] ^ ((~state[15]) & state[16])
+        t20 = state[20] ^ ((~state[21]) & state[22])
+        t21 = state[21] ^ ((~state[22]) & state[23])
+        t22 = state[22] ^ ((~state[23]) & state[24])
+        t23 = state[23] ^ ((~state[24]) & state[20])
+        t24 = state[24] ^ ((~state[20]) & state[21])
+
+        state[0]  = t0
+        state[1]  = t1
+        state[2]  = t2
+        state[3]  = t3
+        state[4]  = t4
+        state[5]  = t5
+        state[6]  = t6
+        state[7]  = t7
+        state[8]  = t8
+        state[9]  = t9
+        state[10] = t10
+        state[11] = t11
+        state[12] = t12
+        state[13] = t13
+        state[14] = t14
+        state[15] = t15
+        state[16] = t16
+        state[17] = t17
+        state[18] = t18
+        state[19] = t19
+        state[20] = t20
+        state[21] = t21
+        state[22] = t22
+        state[23] = t23
+        state[24] = t24
+
+        state[0] ^= _KECCAK_RC[i]
+
+    return state
+
+def _absorb(state, rate, data, buf, buf_idx):
+    """
+    Absorbs input data into the sponge construction.
+
+    Args:
+        state (np.ndarray): The state array of the SHA-3 sponge construction
+        rate (int): The rate of the sponge function
+        data (bytes): The input data to be absorbed
+        buf (np.ndarray): The buffer to absorb the input into
+        buf_idx (int): Current index in the buffer
+
+    Returns:
+        np.ndarray: The updated state array
+        np.ndarray: The updated buffer
+        int: The updated buffer index
+    """
+    todo = len(data)
+    i = 0
+    while todo > 0:
+        cando = rate - buf_idx
+        willabsorb = min(cando, todo)
+        buf[buf_idx:buf_idx + willabsorb] ^= \
+            np.frombuffer(data[i:i+willabsorb], dtype=np.uint8)
+        buf_idx += willabsorb
+        if buf_idx == rate:
+            state, buf, buf_idx = _permute(state, buf, buf_idx)
+        todo -= willabsorb
+        i += willabsorb
+
+    return state, buf, buf_idx
+
+def _squeeze(state, bit_length, rate, buf, buf_idx):
+    """
+    Performs the squeeze operation of the sponge construction
+
+    Args:
+        state (np.ndarray): The state array of the SHA-3 sponge construction
+        bit_length (int): The desired bit length of the hash output
+        rate (int): The rate of the sponge function
+        buf (np.ndarray): The buffer to squeeze the output into
+        buf_idx (int): Current index in the buffer
+
+    Returns:
+        np.ndarray: The hash output
+    """
+    tosqueeze = bit_length // 8
+    output_bytes = np.empty(tosqueeze, dtype=np.uint8)  # Temporary storage for output bytes
+    output_index = 0  # Tracks where to insert bytes into output_bytes
+
+    while tosqueeze > 0:
+                # Calculate how many bytes can be squeezed from the current buffer
+        cansqueeze = rate - buf_idx
+        willsqueeze = min(cansqueeze, tosqueeze)
+
+        # Extract a block of bytes from the state (as uint8 view)
+        start_idx = buf_idx // 8  # Starting 64-bit word index
+        end_idx = (buf_idx + willsqueeze + 7) // 8  # End 64-bit word index (rounded up)
+        
+        # Get the view of the state as uint8 array and slice the appropriate section
+        state_bytes = state[start_idx:end_idx].view(np.uint8)
+
+        # Determine exact byte range to copy, considering buf_idx alignment
+        byte_start = buf_idx % 8
+        byte_end = byte_start + willsqueeze
+        
+        # Copy the sliced part of state to output
+        output_bytes[output_index:output_index + willsqueeze] = state_bytes[byte_start:byte_end]
+
+        # Update the buffer index and output index
+        buf_idx += willsqueeze
+        output_index += willsqueeze
+
+        # If the buffer is fully squeezed, permute the state
+        if buf_idx == rate:
+            state, buf, buf_idx = _permute(state, buf, 0)  # Reset buf_idx after permute
+
+        tosqueeze -= willsqueeze
+
+    return output_bytes
+
+def _pad(state, rate, buf, buf_idx):
+    """
+    Pads the input data in the buffer.
+
+    Args:
+        state (np.ndarray): The state array of the SHA-3 sponge construction
+        rate (int): The rate of the sponge function
+        buf (np.ndarray): The buffer to pad the input into
+        buf_idx (int): Current index in the buffer
+
+    Returns:
+        np.ndarray: The updated state array
+        np.ndarray: The updated buffer
+        int: The updated buffer index
+    """
+    buf[buf_idx] ^= _DSBYTE
+    buf[rate - 1] ^= 0x80
+    return _permute(state, buf, buf_idx)
+
+def _permute(state, buf, buf_idx):
+    """
+    Permutes the internal state and buffer for thorough mixing.
+
+    Args:
+        state (np.ndarray): The state array of the SHA-3 sponge construction
+        buf (np.ndarray): The buffer to permute
+        buf_idx (int): Current index in the buffer
+
+    Returns:
+        np.ndarray: The updated state array
+        np.ndarray: The updated buffer
+        int: The updated buffer index
+    """
+    # Process bytes to uint64
+    state ^= buf.view(dtype=np.uint64)
+
+    # Perform Keccak permutation
+    state = _keccak_f(state)
+
+    # Reset buf_idx and buf
+    buf_idx = 0
+    buf[:] = 0
+
+    return state, buf, buf_idx
+
+def sha3(bit_length, data=b''):
+    """
+    Compute the SHA-3 hash of the input data.
+
+    Args:
+        bit_length (int): The bit length of the hash.
+        data (bytes): The input data to hash.
+
+    Returns:
+        bytes: The hash of the input data.
 
     """
+    rate_map = {224: 144, 256: 136, 384: 104, 512: 72}
+    if bit_length not in rate_map:
+        raise ValueError('Invalid bit length.')
 
-    def __init__(self, bit_length, b=''):
-        rate_map = {224: 144, 256: 136, 384: 104, 512: 72}
-        if bit_length not in rate_map:
-            raise ValueError("Invalid bit length.")
+    rate =  rate_map[bit_length]
+    buf_idx = 0
+    state = np.zeros(25, dtype=np.uint64)
+    buf = np.zeros(200, dtype=np.uint8)
 
-        self.bit_length = bit_length
-        rate = rate_map[bit_length]
-        self.rate, self.dsbyte, self.i = rate, _DSBYTE, 0
-        self.state = np.zeros(25, dtype=np.uint64)
-        self.buf = np.zeros(200, dtype=np.uint8)
-        self._absorb(b)
-        self.direction = _SPONGE_ABSORBING
+    # Absorb data
+    state, buf, buf_idx = _absorb(state, rate, data, buf, buf_idx)
 
-    def update(self, b):
-        """
-        Absorb additional input data.
+    # Pad in preparation for squeezing
+    state, buf, buf_idx = _pad(state, rate, buf, buf_idx)
 
-        Args:
-            b (bytes): The input data to be absorbed.
-
-        Returns:
-            Sha3: The updated Sha3 object.
-
-        """
-        if self.direction == _SPONGE_SQUEEZING:
-            self._permute()
-        self._absorb(b)
-        return self
-
-    def digest(self):
-        """
-        Return the hash value as bytes.
-
-        Returns:
-            bytes: The hash value.
-
-        """
-        if self.direction == _SPONGE_ABSORBING:
-            self._pad()
-        return self._squeeze((200 - self.rate) // 2)
-
-    def hexdigest(self):
-        """
-        Return the hash value as a hexadecimal string.
-
-        Returns:
-            str: The hash value as a hexadecimal string.
-
-        """
-        return self.digest().hex()
-
-    def _keccak_f(self):
-        """
-        The keccak_f permutation function.
-
-        """
-        state = self.state
-        bc = np.zeros(5, dtype=np.uint64)
-
-        for i in range(24):
-            # Parity
-            for x in range(5):
-                bc[x] = 0
-                for y in range(0, 25, 5):
-                    bc[x] ^= state[x + y]
-
-            # Theta
-            for x in range(5):
-                t = bc[(x + 4) % 5] ^ rol(bc[(x + 1) % 5], 1)
-                for y in range(0, 25, 5):
-                    state[y + x] ^= t
-
-            # Rho and pi
-            t = state[1]
-            for x in range(24):
-                bc[0] = state[_KECCAK_PI[x]]
-                state[_KECCAK_PI[x]] = rol(t, _KECCAK_RHO[x])
-                t = bc[0]
-
-            for y in range(0, 25, 5):
-                for x in range(5):
-                    bc[x] = state[y + x]
-                for x in range(5):
-                    state[y + x] = bc[x] ^ ((~bc[(x + 1) % 5]) & bc[(x + 2) % 5])
-
-            state[0] ^= _KECCAK_RC[i]
-        self.state = state
-
-    def _absorb(self, b):
-        """
-        Absorb input data into the sponge construction.
-
-        Args:
-            b (bytes): The input data to be absorbed.
-
-        """
-        todo = len(b)
-        i = 0
-        while todo > 0:
-            cando = self.rate - self.i
-            willabsorb = min(cando, todo)
-            self.buf[self.i:self.i + willabsorb] ^= \
-                np.frombuffer(b[i:i+willabsorb], dtype=np.uint8)
-            self.i += willabsorb
-            if self.i == self.rate:
-                self._permute()
-            todo -= willabsorb
-            i += willabsorb
-
-    def _squeeze(self, n):
-        """
-        Squeeze output data from the sponge construction.
-
-        Args:
-            n (int): The number of bytes to squeeze.
-
-        Returns:
-            bytes: The squeezed output data.
-
-        """
-        tosqueeze = n
-        b = b''
-        while tosqueeze > 0:
-            cansqueeze = self.rate - self.i
-            willsqueeze = min(cansqueeze, tosqueeze)
-            b += self.state.view(dtype=np.uint8)[self.i:self.i + willsqueeze].tostring()
-            self.i += willsqueeze
-            if self.i == self.rate:
-                self._permute()
-            tosqueeze -= willsqueeze
-        return b
-
-    def _pad(self):
-        """
-        Pad the input data in the buffer.
-
-        """
-        self.buf[self.i] ^= self.dsbyte
-        self.buf[self.rate - 1] ^= 0x80
-        self._permute()
-
-    def _permute(self):
-        """
-        Permute the internal state and buffer.
-
-        """
-        self.state ^= self.buf.view(dtype=np.uint64)
-        self._keccak_f()
-        self.i = 0
-        self.buf[:] = 0
-
-    def __repr__(self):
-        return f'Sha3(bits={self.bit_length}, rate={self.rate}, dsbyte=0x{self.dsbyte:02x})'
+    # Squeeze the hash
+    return _squeeze(state, bit_length, rate, buf, buf_idx)
--- test/benchmark.py
+++ test/benchmark.py
@@ -8,13 +8,11 @@ from sha3 import sha3_python, sha3_numba, sha3_cuda
 
 def benchmark_and_verify(bit_length, input_bytes, num_iterations, use_cuda=False):
 
-    # Python SHA3 Implementation (OOP)
+    # Python SHA3 Implementation
     start_time = time.perf_counter()
     for _ in range(num_iterations):
-        sha3_256 = sha3_python(bit_length)
-        sha3_256.update(input_bytes)
-        my_hash = sha3_256.hexdigest()
-    my_time = time.perf_counter() - start_time
+        python_hash = sha3_python(bit_length, input_bytes).tobytes().hex()
+    python_time = time.perf_counter() - start_time
 
     # Python SHA3 Implementation (Numba)
     sha3_numba(bit_length, input_bytes) # Warm up
@@ -46,14 +44,14 @@ def benchmark_and_verify(bit_length, input_bytes, num_iterations, use_cuda=False
     lib_time = time.perf_counter() - start_time
     
     print(f"Input Length: {len(input_bytes)} bytes, Iterations: {num_iterations}")
-    print(f"Python  SHA3-{bit_length} Time: {my_time:.4f} s, Hashes/s: {num_iterations/my_time:.1f}, Length: {len(my_hash)}, Hash: {my_hash}")
+    print(f"Python  SHA3-{bit_length} Time: {python_time:.4f} s, Hashes/s: {num_iterations/python_time:.1f}, Length: {len(python_hash)}, Hash: {python_hash}")
     print(f"Numba   SHA3-{bit_length} Time: {numba_time:.4f} s, Hashes/s: {num_iterations/numba_time:.1f}, Length: {len(numba_hash)}, Hash: {numba_hash}")
     if use_cuda:
         print(f"CUDA    SHA3-{bit_length} Time: {cuda_time:.4f} s, Hashes/s: {num_iterations/cuda_time:.1f}, Length: {len(cuda_hash)}, Hash: {cuda_hash}")
     print(f"HashLib SHA3-{bit_length} Time: {lib_time:.4f} s, Hashes/s: {num_iterations/lib_time:.1f}, Length: {len(lib_hash)}, Hash: {lib_hash}")
 
     # Verify Outputs
-    assert my_hash == lib_hash, f"Mismatch: {my_hash} != {lib_hash}"
+    assert python_hash == lib_hash, f"Mismatch: {python_hash} != {lib_hash}"
     assert numba_hash == lib_hash, f"Mismatch: {numba_hash} != {lib_hash}"
     if use_cuda:
         assert cuda_hash == lib_hash, f"Mismatch: {cuda_hash} != {lib_hash}"
